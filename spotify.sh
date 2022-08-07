a0c3010d844ff99443cc5783bee9a49b95c70b9e
Rin <hdymkylf@gmail.com>
2022-08-07 10:35:45 +0700

    Fix & Improvement

--- spotify.sh
+++ spotify.sh
@@ -16,14 +16,14 @@ command -v 'unzip' >/dev/null || { echo -e "\n[unzip] isn't installed!\n"; sudo 
 
 SPOTIFY=$(command -v 'spotify')
 
-SPDIR='/usr/share/spotify'
-SPCONF="${HOME}/.config/spotify" || SPCONF='~/.config/spotify'
-SPPREFS="${SPCONF}/prefs"
+sp_dir='/usr/share/spotify'
+sp_conf="${HOME}/.config/spotify" || sp_conf='~/.config/spotify'
+sp_prefs="${sp_conf}/prefs"
 
 # remove previously installed spotify (avoid spicetify unable to backup spotify)
 if [ -n "$SPOTIFY" ]; then
 	sudo apt -y update && sudo apt-get -y purge --auto-remove 'spotify-client'
-	[ -d "$SPCONF" ] && sudo rm -rf "$SPCONF"
+	[ -d "$sp_conf" ] && sudo rm -rf "$sp_conf"
 	# sleep to make sure commands are executed properly
 	sleep 3
 fi
@@ -44,8 +44,8 @@ if [ ! -f "/usr/share/applications/spotify.desktop" ]; then
 	[ -z "$(command -v 'xfce4-panel')" ] || xfce4-panel -r
 fi
 
-# set spotify permissions
-sudo chmod a+rw -R "$SPDIR"
+# set spotify permissions recursively
+sudo chmod a+rw -R "$sp_dir"
 
 # sleep to make sure commands are executed properly
 sleep 3
@@ -56,8 +56,14 @@ sleep 3
 
 SPICETIFY=$(command -v 'spicetify')
 
-SPCDIR="${HOME}/.spicetify" || SPCDIR='~/.spicetify'
-SPCCONF="${HOME}/.config/spicetify" || SPCCONF='~/.config/spicetify'
+spc_dir="${HOME}/.spicetify" || spc_dir='~/.spicetify'
+spc_exe="${spc_dir}/spicetify"
+spc_conf="${HOME}/.config/spicetify" || spc_conf='~/.config/spicetify'
+
+# remove previously installed spicetify (if installed)
+[ -z "$SPICETIFY" ] || spicetify restore
+[ -d "$spc_dir" ] && sudo rm -rf "$spc_dir"
+[ -d "$spc_conf" ] && sudo rm -rf "$spc_conf"
 
 # check platform
 case $(uname -sm) in
@@ -68,44 +74,77 @@ case $(uname -sm) in
 	*) echo "Unsupported platform $(uname -sm). Only avaible for x86_64 and arm64 binaries for Linux and Darwin."; exit 1 ;;
 esac
 
-# version
-version='v2.9.9'
+##############################
+# fetch and download spicetify
+##############################
 
-DOWNDIR="${HOME}/Downloads" || DOWNDIR='~/Downloads'
-SPCURL="https://github.com/spicetify/spicetify-cli/releases/download/${version}/spicetify-${version#v}-${target}.tar.gz"
-SPCFILE="${DOWNDIR}/${SPCURL##*/}"
+# check spotify version
+sp_ver=$(spotify --version)
+sp_ver=${sp_version%.*.*}
+sp_ver=${sp_version#*.*.}
 
-# remove previously installed spicetify (if installed)
-[ -z "$SPICETIFY" ] || spicetify restore
-[ -d "$SPCDIR" ] && sudo rm -rf "$SPCDIR"
-[ -d "$SPCCONF" ] && sudo rm -rf "$SPCCONF"
+releases_url='https://github.com/spicetify/spicetify-cli/releases'
 
-# fetch & download spicetify
-wget --directory-prefix="$DOWNDIR" "$SPCURL"
+# use latest spicetify version if spotify version newer than v1.1.84
+[ "$sp_ver" -gt "84" ] && {
+    tag=$(curl -LsH 'Accept: application/json' ${releases_url}/latest)
+    tag=${tag%\"\,\"update_url*}
+    tag=${tag#*tag_name\":\"}
+}
+# use spicetify v2.9.9 if spotify version is v1.1.84
+[ "$sp_ver" == "84" ] && tag='v2.9.9'
+# use spicetify v2.9.9 if spotify version earlier than v1.1.84
+[ "$sp_ver" -lt "84" ] && tag='v2.9.5'
+
+download_url="${releases_url}/download/${tag}/spicetify-${tag#v}-${target}.tar.gz"
+download_dir="${HOME}/Downloads" || download_dir='~/Downloads'
+
+# downloaded file
+download_output="${download_dir}/${download_url##*/}"
+
+# download file (if not already downloaded) & set it executable
+[ -f "$download_output" ] || wget --directory-prefix="$download_dir" $download_url
 
 # sleep to make sure commands are executed properly
 sleep 3
 
 # create spicetify directory
-sudo mkdir -p "$SPCDIR"
+mkdir -p "$spc_dir"
 
 # extract spicetify archive, and remove it after extracted
-sudo tar -zxf "$SPCFILE" -C "$SPCDIR" && rm -f "$SPCFILE"
+sudo tar -zxf "$download_output" -C "$spc_dir" && rm -f "$download_output"
 
 # set spicetify permissions recursively
-sudo chmod a+rwx -R "$SPCDIR"
+chmod a+rwx -R "$spc_dir"
 
-# remove & add spicetify to $PATH
-if [ -f "~/.bash_profile" ] || [ -f "~/.bash_login" ]; then
-	grep -v '.spicetify' $HOME/.bashrc > $HOME/.bashrc.tmp && cp -af $HOME/.bashrc.tmp $HOME/.bashrc
-	echo 'export PATH="$PATH:$HOME/.spicetify"' >> $HOME/.bashrc && source $HOME/.bashrc
-else
-	grep -v '.spicetify' $HOME/.profile > $HOME/.profile.tmp && cp -af $HOME/.profile.tmp $HOME/.profile
-	echo 'export PATH="$PATH:$HOME/.spicetify"' >> $HOME/.profile && source $HOME/.profile
-fi
+##############################
+# add spicetify to $PATH
+##############################
 
-# set executable permissions & run spicetify
-sudo chmod a+x "${SPCDIR}/spicetify" && spicetify
+case $SHELL in
+	*bash) 
+		[ -f "${HOME}/.bashrc" ] && shell_rc='.bashrc'
+		[ -f "${HOME}/.bash_profile" ] && shell_rc='.bash_profile'
+	;;
+	*csh) 
+		[ -f "${HOME}/.cshrc" ] && shell_rc='.cshrc'
+		[ -f "${HOME}/.login" ] && shell_rc='.login'
+	;;
+	*ksh) shell_rc='.profile' ;;
+	*zsh) shell_rc='.zshrc' ;;
+	*) [ -f "${HOME}/.bash_profile" ] || shell_rc='.profile';;
+esac
+
+shell_rc="${HOME}/${shell_rc}"
+grep -v "${spc_dir##*/}" "$shell_rc" > ${shell_rc}.tmp && mv -f "${shell_rc}.tmp" "$shell_rc"
+echo -e "\nexport PATH="\$PATH:${spc_dir}"" >> $shell_rc && { source "$shell_rc" || . "$shell_rc"; }
+echo -e "\nexport SPICETIFY_INSTALL="${spc_dir}"" >> $shell_rc && { source "$shell_rc" || . "$shell_rc"; }
+
+# set executable permissions
+chmod a+x "$spc_exe"
+
+# run spicetify
+spicetify || . $spc_exe
 
 # sleep to make sure commands are executed properly
 sleep 3
@@ -115,23 +154,23 @@ sleep 3
 ##################################################
 
 # backup spotify
-spicetify backup apply
+spicetify backup apply || . $spc_exe backup apply
 
 # apply theme config
-spicetify config inject_css 1 overwrite_assets 1 replace_colors 1
+spicetify config inject_css 1 overwrite_assets 1 replace_colors 1 || . $spc_exe config inject_css 1 overwrite_assets 1 replace_colors 1
 
 # additional config
-spicetify config experimental_features 0 check_spicetify_upgrade 0
+spicetify config experimental_features 0 check_spicetify_upgrade 0 || . $spc_exe config experimental_features 0 check_spicetify_upgrade 0
 
 # lyrics integration
-spicetify config extensions popupLyrics.js
-spicetify config custom_apps lyrics-plus
+spicetify config extensions popupLyrics.js || . $spc_exe config extensions popupLyrics.js
+spicetify config custom_apps lyrics-plus || . $spc_exe config custom_apps lyrics-plus
 
 # install spicetify marketplace
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
 
 # apply configuration
-spicetify apply
+spicetify apply || . $spc_exe apply
 
 # sleep to make sure commands are executed properly
 sleep 3
