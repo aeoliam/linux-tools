065c06b36091ed66213a28dae126242bf9626f70
Rin <hdymkylf@gmail.com>
2022-08-17 00:34:20 +0700

    Add new line to message

--- reboot_required.sh
+++ reboot_required.sh
@@ -2,7 +2,7 @@
 # by @aeoliam
 
 if [ -f '/var/run/reboot-required' ]; then
-	echo -e "─ Reboot required! Reboot now? [Y/n]"
+	echo -e "\n\n─ Reboot required! Reboot now? [Y/n]"
 	read reboot_choice
 	{ [ "$reboot_choice" == "y" ] || [ "$reboot_choice" == "Y" ]; } && sudo reboot -f
 fi
