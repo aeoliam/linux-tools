84acc385ddef377565f569477089bf1dcb25faa3
Rin <hdymkylf@gmail.com>
2022-08-17 04:35:14 +0700

    Fixes & Improvements

--- spotify.sh
+++ spotify.sh
@@ -4,11 +4,13 @@
 # SETUP
 ##################################################
 
-# setup dependencies
+# install dependencies
 command -v 'curl' >/dev/null || { printf "\n[curl] isn't installed!\n"; sudo apt -y update && sudo apt -y install 'curl'; }
 command -v 'wget' >/dev/null || { printf "\n[grep] isn't installed!\n"; sudo apt -y update && sudo apt -y install 'wget'; }
 command -v 'tar' >/dev/null || { printf "\n[tar] isn't installed!\n"; sudo apt -y update && sudo apt -y install 'tar'; }
 command -v 'unzip' >/dev/null || { printf "\n[unzip] isn't installed!\n"; sudo apt -y update && sudo apt -y install 'unzip'; }
+
+wait
 
 ##################################################
 # INSTALL SPOTIFY
@@ -24,9 +26,9 @@ sp_prefs="${sp_conf}/prefs"
 if [ -n "$SPOTIFY" ]; then
 	sudo apt -y update && sudo apt-get -y purge --auto-remove 'spotify-client'
 	[ -d "$sp_conf" ] && sudo rm -rf "$sp_conf"
-	# sleep to make sure commands are executed properly
-	sleep 3
 fi
+
+wait
 
 # add spotify pubkey to apt
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
@@ -34,8 +36,8 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo 
 sudo echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.dspotify.list
 # finally install spotify-client
 sudo apt -y update && sudo apt -y install 'spotify-client'
-# sleep to make sure commands are executed properly
-sleep 3
+
+wait
 
 # create spotify (linked) shortcut (if not exist)
 if [ ! -f "/usr/share/applications/spotify.desktop" ]; then
@@ -47,8 +49,7 @@ fi
 # set spotify permissions recursively
 sudo chmod a+rw -R "$sp_dir"
 
-# sleep to make sure commands are executed properly
-sleep 3
+wait
 
 ##################################################
 # INSTALL SPICETIFY
@@ -64,6 +65,8 @@ spc_conf="${HOME}/.config/spicetify" || spc_conf='~/.config/spicetify'
 [ -z "$SPICETIFY" ] || spicetify restore
 [ -d "$spc_dir" ] && sudo rm -rf "$spc_dir"
 [ -d "$spc_conf" ] && sudo rm -rf "$spc_conf"
+
+wait
 
 # check platform
 case $(uname -sm) in
@@ -87,9 +90,9 @@ releases_url='https://github.com/spicetify/spicetify-cli/releases'
 
 # use latest spicetify version if spotify version newer than v1.1.84
 [ "$sp_ver" -gt "84" ] && {
-    tag=$(curl -LsH 'Accept: application/json' ${releases_url}/latest)
-    tag=${tag%\"\,\"update_url*}
-    tag=${tag#*tag_name\":\"}
+	tag=$(curl -LsH 'Accept: application/json' ${releases_url}/latest)
+	tag=${tag%\"\,\"update_url*}
+	tag=${tag#*tag_name\":\"}
 }
 # use spicetify v2.9.9 if spotify version is v1.1.84
 [ "$sp_ver" == "84" ] && tag='v2.9.9'
@@ -105,8 +108,7 @@ download_output="${download_dir}/${download_url##*/}"
 # download file (if not already downloaded) & set it executable
 [ -f "$download_output" ] || wget --directory-prefix="$download_dir" $download_url
 
-# sleep to make sure commands are executed properly
-sleep 3
+wait
 
 # create spicetify directory
 mkdir -p "$spc_dir"
@@ -116,6 +118,8 @@ sudo tar -zxf "$download_output" -C "$spc_dir" && rm -f "$download_output"
 
 # set spicetify permissions recursively
 chmod a+rwx -R "$spc_dir"
+
+wait
 
 ##############################
 # add spicetify to $PATH
@@ -136,44 +140,50 @@ case $SHELL in
 esac
 
 shell_rc="${HOME}/${shell_rc}"
-grep -v "${spc_dir##*/}" "$shell_rc" > ${shell_rc}.tmp && mv -f "${shell_rc}.tmp" "$shell_rc"
-printf "\nexport PATH="\$PATH:${spc_dir}"" >> $shell_rc && { source "$shell_rc" 2>/dev/null || . "$shell_rc" 2>/dev/null; }
-#printf "\nexport SPICETIFY_INSTALL="${spc_dir}"" >> $shell_rc && { source "$shell_rc" 2>/dev/null || . "$shell_rc" 2>/dev/null; }
+
+cat "$shell_rc" | grep "${spc_dir##*/}" && { cat "$shell_rc" | grep -v "${spc_dir##*/}" > "${shell_rc}.tmp" && mv -f "${shell_rc}.tmp" "$shell_rc"; }
+
+printf "\nexport PATH="\$PATH:${spc_dir}"\n" >> $shell_rc && { source "$shell_rc" 2>/dev/null || . "$shell_rc" 2>/dev/null; }
+printf "\nexport SPICETIFY_INSTALL="${spc_dir}"\n" >> $shell_rc && { source "$shell_rc" 2>/dev/null || . "$shell_rc" 2>/dev/null; }
 
 # set executable permissions
-chmod a+x "$spc_exe"
+chmod a+rwx -R "$spc_dir"
 
 # run spicetify
-spicetify || . $spc_exe
+spicetify 2>/dev/null || . "$spc_exe"
 
-# sleep to make sure commands are executed properly
-sleep 3
+wait
 
 ##################################################
 # SPICETIFY CONFIGURATION
 ##################################################
 
 # backup spotify
-spicetify backup apply || . $spc_exe backup apply
+spicetify backup apply 2>/dev/null || . "$spc_exe" backup apply
+
+wait
 
 # apply theme config
-spicetify config inject_css 1 overwrite_assets 1 replace_colors 1 || . $spc_exe config inject_css 1 overwrite_assets 1 replace_colors 1
+spicetify config inject_css 1 overwrite_assets 1 replace_colors 1 2>/dev/null || . "$spc_exe" config inject_css 1 overwrite_assets 1 replace_colors 1
 
 # additional config
-spicetify config experimental_features 0 check_spicetify_upgrade 0 || . $spc_exe config experimental_features 0 check_spicetify_upgrade 0
+spicetify config experimental_features 0 check_spicetify_upgrade 0 2>/dev/null || . "$spc_exe" config experimental_features 0 check_spicetify_upgrade 0
 
 # lyrics integration
-spicetify config extensions popupLyrics.js || . $spc_exe config extensions popupLyrics.js
-spicetify config custom_apps lyrics-plus || . $spc_exe config custom_apps lyrics-plus
+spicetify config extensions popupLyrics.js 2>/dev/null || . "$spc_exe" config extensions popupLyrics.js
+spicetify config custom_apps lyrics-plus 2>/dev/null || . "$spc_exe" config custom_apps lyrics-plus
 
 # install spicetify marketplace
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
 
-# apply configuration
-spicetify apply || . $spc_exe apply
+wait
 
-# sleep to make sure commands are executed properly
-sleep 3
+# apply configuration
+spicetify apply 2>/dev/null || . "$spc_exe" apply
+
+wait
+
+printf "\n\nRestart your terminal if you want to run spicetify commands.\n"
 
 ##################################################
 # END OF SCRIPT
