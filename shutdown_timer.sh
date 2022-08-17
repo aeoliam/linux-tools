a599c78090cedf7a407b828eaca2dc2f1a75cb00
Rin <hdymkylf@gmail.com>
2022-08-17 21:33:52 +0700

    Universal usage

--- shutdown_timer.sh
+++ shutdown_timer.sh
@@ -4,4 +4,4 @@ echo "Input shutdown timer. (in seconds)"
 read shutdown_time
 total_time=$(seq 1 $shutdown_time)
 for countdown in $total_time; do echo -en "[${countdown}]\r" && sleep 1; done
-xfce4-session-logout --halt --fast
+shutdown --halt || reboot --halt
