b008b933da42424788d140a0daeeb1431e08312c
Rin <hdymkylf@gmail.com>
2022-08-28 16:58:53 +0700

    Update functions, improvements & fixes.

--- uninstaller.sh
+++ uninstaller.sh
@@ -4,46 +4,25 @@
 ##################################################
 # FUNCTIONS
 ##################################################
-
-apt_install() {
-	sudo apt -y update
+ui_print() { printf "${1}\n"; sleep 0.5; }
+apt_remove()
+{
+	ui_print "Updating databases..." && sudo apt -y update &>/dev/null
 	sudo apt-get -y purge --auto-remove "$1"
-	[ -z "$2" ] && sudo apt-get -y purge --auto-remove "$2"
-	[ -z "$3" ] && sudo apt-get -y purge --auto-remove "$3"
+	[ -z "$2" ] || sudo apt-get -y purge --auto-remove "$2"
+	[ -z "$3" ] || sudo apt-get -y purge --auto-remove "$3"
 	wait; }
-uninstall_firefox() {
-	sudo apt -y update && { sudo apt-get -y purge --auto-remove '*firefox*'; } || return 1
-	sudo snap remove 'firefox' && sudo rm -rf /root/snap/firefox
+snap_remove()
+{
+	local TARGET=$(printf "$1" | sed 's/*//g')
+	sudo snap remove "$1" && sudo find / -type 'f,d' -name "$TARGET" | while read leftover; do rm -fr "$leftover" 2>/dev/null; done
 }
-uninstall_thunderbird() {
-	sudo apt -y update && { sudo apt-get -y purge --auto-remove '*thunderbird*'; } || return 1
-}
-uninstall_libreoffice() {
-	sudo apt -y update && { sudo apt-get -y purge --auto-remove '*libreoffice*'; } || return 1
-}
-uninstall_vim() {
-	sudo apt -y update && { sudo apt-get -y purge --auto-remove 'vim*'; sudo apt-get -y purge --auto-remove 'xxd'; } || return 1
-}
-uninstall_chrome() {
-	sudo apt -y update && { sudo apt-get -y purge --auto-remove 'google-chrome-*'; } || return 1
-}
-uninstall_vbox() {
-	sudo apt -y update && {
-		sudo apt-get -y purge --auto-remove 'dkms'
-		sudo apt-get -y purge --auto-remove 'virtualbox'
-		sudo apt-get -y purge --auto-remove 'virtualbox-ext-pack'
-	} || return 1
-}
-uninstall_fonts() {
-	sudo apt -y update && { sudo apt-get -y purge --auto-remove 'fonts-*'; } || return 1
-}
-unistall_xfce4() {
-	echo -e "This will remove & replace some xfce4 packages and dependencies." && sleep 0.3
-	echo -e "for more information, see xfce4.txt" && sleep 0.5
-	echo -e "Are you sure? [Y/n]"
+unistall_xfce4()
+{
+	ui_print "This will remove & replace some xfce4 packages and dependencies."
+	ui_print "Are you sure? [Y/n]"
 	read XFCE
-	{ [ "$XFCE" == "Y"] || [ "$XFCE" == "y" ]; } && {
-		sudo apt -y update
+	if [ "$XFCE" == "y"] || [ "$XFCE" == "Y" ]; then
 		local PKGLIST="
 		# xfce4
 		xterm
@@ -64,8 +43,8 @@ unistall_xfce4() {
 		vim*
 		xxd
 		"
-		local PKGLIST=$(echo "$PKGLIST" | grep -v '#')
-		for PKG in $PKGLIST; do sudo apt-get purge --auto-remove "$PKG"; done
+		local PKGLIST=$(printf "$PKGLIST" | grep -v '#')
+		for PKG in $PKGLIST; do apt_remove "$PKG"; done
 		PKGLIST="
 		# Bluetooth
 		bluetooth
@@ -83,25 +62,21 @@ unistall_xfce4() {
 		# fira code fonts
 		fonts-firacode
 		"
-		local PKGLIST=$(echo -n "$PKGLIST" | grp -v '#')
-		for PKG in $PKGLIST; do sudo apt -y install "$PKG"; done
-	} || return 1
-}
-uninstall_all() {
-	uninstall_firefox
-	uninstall_thunderbird
-	uninstall_libreoffice
-	unistall_vim
-	uninstall_chrome
-	uninstall_vbox
+		local PKGLIST=$(printf "$PKGLIST" | grp -v '#')
+		for PKG in $PKGLIST; do sudo apt -y update; sudo apt -y install "$PKG"; done
+	elif [ "$XFCE" == "n" ] || [ "$XFCE" == "N" ]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 ##################################################
 # CLI
 ##################################################
 
-echo -e "────────────────────────────────────────" && sleep 0.5
-echo -e "Package list:"
+ui_print "────────────────────────────────────────" && sleep 0.5
+ui_print "Package list:"
 PKGLIST="
 [1]:Firefox
 [2]:Thunderbird
@@ -109,32 +84,30 @@ PKGLIST="
 [4]:Vim
 [5]:Chrome
 [5]:VirtualBox
-[a]:AllPackagesAbove
 [f]:Fonts
 [x]:xfce4
 [c]:Cancel
 "
-for PKG in $PKGLIST; do echo -e "$PKG" && sleep 0.3; done
-echo -e "Which package's you wish to uninstall?"
+for PKG in $PKGLIST; do ui_print "$PKG" && sleep 0.3; done
+ui_print "Which package's you wish to uninstall?"
 read PACKAGE
 case $PACKAGE in
-	1) uninstall_firefox ;;
-	2) uninstall_thunderbird ;;
-	3) uninstall_libreoffice ;;
-	4) uninstall_vim ;;
-	5) uninstall_chrome ;;
-	6) uninstall_vbox ;;
-	a) uninstall_all ;;
-	f) uninstall_fonts ;;
-	x) uninstall_xfce4 ;;
-	c) exit 0 ;;
-	*) echo -e 'Please input one of the options above!'; exit 1 ;;
+	1) apt_remove '*firefox*' || snap_remove '*firefox*' ;;
+	2) apt_remove '*thunderbird*' || apt_remove '*thunderbird*' ;;
+	3) apt_remove '*libreoffice*' || snap_remove '*libreoffice*' ;;
+	4) { apt_remove 'vim*' && apt_remove 'xxd'; } || { snap_remove 'vim*' && snap_remove 'xxd'; } ;;
+	5) apt_remove 'google-chrome*' || snap_remove 'google-chrome*' ;;
+	6) apt_remove 'dkms' 'virtualbox' 'virtualbox-ext-pack' || snap_remove 'dkms' 'virtualbox' 'virtualbox-ext-pack' ;;
+	f) apt_remove 'fonts-*' || snap_remove 'fonts-*' ;;
+	x) xfce4 ;;
+	c) return 0 ;;
+	*) ui_print 'Please input one of the options above!'; return 1 ;;
 esac
 wait
 
-# perform cleanup
-echo -e "Cleaning Up.."
-curl -fsSL https://raw.githubusercontent.com/aeoliam/linux-tools/master/cleanup.sh | sh &>/dev/null
+# perform cleanupe
+ui_print "Cleaning  Up.."
+curl -fsSL https://raw.githubusercontent.com/aeoliam/linux-tools/master/cleanup.sh &>/dev/null | sh
 wait
 
 # detect whether reboot is required or not
